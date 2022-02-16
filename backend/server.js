5e9a04c07b35484be5b65a2e2c61de6100060b92
Ryan Lee <ryanlee@Ryans-MacBook-Air.local>
2022-02-16 11:05:40 +0900

    Add error & async handler

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,20 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const goalRoutes = require('./routes/goalRoutes.js')
+const { errorHandler } = require('./middleware/errorMiddleware.js')
 
 // Port
 const port = process.env.PORT || 5000
 
 const app = express()
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // Routes
 app.use('/api/goals', goalRoutes)
+app.use(errorHandler)
 
 // Server
 app.listen(port, () => console.log(`Server runnning on port:${port}`))
