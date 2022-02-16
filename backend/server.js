e32568e316abe72b4cfd62ed04da5d7b9a52c680
Ryan Lee <ryanlee@Ryans-MacBook-Air.local>
2022-02-16 10:46:31 +0900

    Add routes & contorllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const goalRoutes = require('./routes/goalRoutes.js')
+
+// Port
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// Routes
+app.use('/api/goals', goalRoutes)
+
+// Server
+app.listen(port, () => console.log(`Server runnning on port:${port}`))
